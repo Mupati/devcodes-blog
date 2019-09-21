92f2760479a66c005859b287048975d544d99f26
Mupati <scientificgh@gmail.com>
2019-09-21 16:44:57 +0000

    Add only reload to mixin

--- mixins/storyblokLivePreview.js
+++ mixins/storyblokLivePreview.js
@@ -1,13 +1,13 @@
 export default {
   mounted() {
-    this.$storybridge.on(['published', 'change'], event => {
-      if (!event.slugChanged) {
-        // Reload the page on save events (publish, save button or autosave)
-        this.$nuxt.$router.go({
-          path: this.$nuxt.$router.currentRoute,
-          force: true
-        })
-      }
+    this.$storybridge.on('change', event => {
+      // if (!event.slugChanged) {
+      //   // Reload the page on save events (publish, save button or autosave)
+      //   this.$nuxt.$router.go({
+      //     path: this.$nuxt.$router.currentRoute,
+      //     force: true
+      //   })
+      // }
       location.reload(true)
     })
   }
